bdf56a9ca9067682a4758e10c80256a2376a7ff5
Saadia Shahid <saadia.shahid@torontomu.ca>
2024-08-04 13:49:44 -0400

    meta-data

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Saadia's Games",
   description: "Your favourite games now in Squid Game style",
+
 };
 
 export default function RootLayout({
@@ -24,6 +25,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" >
+      <head>
+        <meta property="og:title" content="Saadia's Games" />
+        <meta property="og:description" content="Your favourite games now in Squid Game style" />
+        <meta property="og:image" content="/images/splash-screen.png" />
+        <meta property="og:url" content="https://saadias-games.vercel.com" />
+        {/* <meta name="twitter:card" content="summary_large_image" /> */}
+      </head>
       <body className={inter.className}>
         {children}
       </body>
